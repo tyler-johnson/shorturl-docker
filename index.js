5a67f90e47b723a19de055140fad08f06e5aed5a
Tyler Johnson <tyler@vintyge.com>
2016-02-28 12:12:12 -0700

    disable x-powered-by

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var title = process.env.SHORTURL_TITLE;
 process.env.NODE_ENV = "production";
 
-require("@mrgalaxy/shorturl")({
+var shorturl = require("@mrgalaxy/shorturl")({
 	title: title,
 	redis: {
 		host: process.env.DB_PORT_6379_TCP_ADDR,
 		port: process.env.DB_PORT_6379_TCP_PORT
 	}
-}).listen(80, function() {
+});
+
+shorturl.disable("x-powered-by");
+
+shorturl.listen(80, function() {
 	var addr = this.address();
 	console.log("Short URL service%s listening at http://%s:%s", title ? " '" + title + "'" : "", addr.address, addr.port);
 	console.log("Enter Ctrl-C to stop the server.");
